8b9e611eab48e8e0d4e17ce1727b4fc386080098
Shovan Nag <shovanr06@gmail.com>
2024-09-07 15:09:36 +0600

    variable and data type

--- Program.cs
+++ Program.cs
@@ -2,7 +2,23 @@
 
 class Test{
     public static void Main(string[] args){
-        Console.WriteLine(123);
-        Console.WriteLine("Testing");
+        // Console.WriteLine(123);
+        // Console.WriteLine("Testing");
+        // Console.WriteLine("Enter a character");
+        // var test = Console.Read();
+        // Console.WriteLine(test);
+
+        string name = "Shovan Nag";
+        int age = 32;
+
+        Console.WriteLine(name);
+        Console.WriteLine(age);
+
+        name = "John Doe";
+        Console.WriteLine(name);
+
+        age = 21;
+        Console.WriteLine(age);
+
     }
 }
